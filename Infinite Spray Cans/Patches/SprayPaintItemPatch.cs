500afce107d5335dfa9b1769b4d8910ac6b34148
twoandtwelve <jackykim573@gmail.com>
2024-03-05 14:52:03 +1300

    Add project files.

---
+++ Infinite Spray Cans/Patches/SprayPaintItemPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InfiniteSprayCans.Patches
+{
+    [HarmonyPatch(typeof(SprayPaintItem))]
+
+    internal class SprayPaintItemPatch
+    {
+        [HarmonyPatch("LateUpdate")]
+        [HarmonyPostfix]
+        static void infiniteSprayCansPatch(ref float ___sprayCanTank, ref float ___sprayCanShakeMeter)
+        {
+            ___sprayCanTank = 1f;
+            ___sprayCanShakeMeter = 1f;
+        }
+    }
+}
---
+++ Infinite Spray Cans/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniteSprayCans
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class InfiniteSprayCans : BaseUnityPlugin
+    {
+        private const string modGUID = "Jacky.InfiniteSprayCans";
+        private const string modName = "Infinite Spray Cans";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static InfiniteSprayCans Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("The mod has awaken");
+
+            harmony.PatchAll();
+        }
+    }
+}
